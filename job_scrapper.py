69fcc9d674dfd9cc213bb35927c2965b2dbef1f9
Easymean1207 <dlwlasl1207@gmail.com>
2023-09-01 15:21:16 +0900

    노마드코더 python-webScraper #5.5 keyword Arguments 완료

--- job_scrapper.py
+++ job_scrapper.py
@@ -1,9 +1,18 @@
-from bs4 import BeautifulSoup
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
 
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+    print("Can't request website")
+    exit(1)
+else:
+    # print(response.text)  # show html code of website included in response
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all("section", class_="jobs")  # use keyword Arguments
 
 """ using try-catch(except) """
 # try:
@@ -12,13 +21,3 @@ search_term = "python"
 #     print(response.text)  # Show HTML code of website included in the response
 # except Exception as e:
 #     print("Can't request website:", e)
-
-response = get(f"{base_url}+{search_term}")
-
-if response.status_code != 200:
-    print("Can't request website")
-    exit(1)
-else:
-    # print(response.text)  # show html code of website included in response
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all("section", class_="jobs")
