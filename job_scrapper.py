33b4ee36e8dd336048b766a6bbca4c218d5c08b5
Easymean1207 <dlwlasl1207@gmail.com>
2023-09-23 18:51:44 +0900

    노마드코더 python-webScraper #5.14 Pages 완료

--- job_scrapper.py
+++ job_scrapper.py
@@ -11,14 +11,33 @@ browser = webdriver.Chrome()
 browser.get(base_url + search_term)
 # print(browser.page_source)
 
+results = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_="css-zu9cdh")
+job_list = soup.find("ul", class_="css-zu9cdh")  # use find method
 jobs = job_list.find_all("li", recursive=False)  # only search direct li
 
 for job in jobs:
-    print(job)
-    print("////////////")
+    zone = job.find("div", class_="mozaic-zone")
+    if zone == None:  # None is python's Null type
+        anchor = job.select_one("h2 a")  # use select method (using css selector)
+        if anchor != None:
+            # data extraction from jobTitle class
+            title = anchor["aria-label"]
+            link = anchor["href"]
+            company = job.find("span", class_="companyName")
+            location = job.find("div", class_="companyLocation")
 
+            # input extracted data into job_data
+            job_data = {
+                "link": f"https://kr.indeed.com{link}",
+                "company": company.string,
+                "location": location.string,
+                "position": title,  # position is aria-label
+            }
+            results.append(job_data)
+
+for result in results:
+    print(result, "\n///////\n")
 
 while True:
     pass
